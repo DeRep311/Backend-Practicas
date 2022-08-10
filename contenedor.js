b9de1c216a18cfde1e15baf3e5e54e78589ad4a2
DeRep311 <77757535+DeRep311@users.noreply.github.com>
2022-08-10 21:33:42 +0000

    Update contenedor.js

--- contenedor.js
+++ contenedor.js
@@ -1,4 +1,5 @@
-const { promises: fs } = require('fs')
+const { promises: fs } = require('fs');
+
 
 class Contenedor {
     constructor(ruta) {
@@ -7,65 +8,88 @@ class Contenedor {
 
     async save(data) {
 
-        let objet = await this.getAll();
-        let newId;
-        if (objet.lenght == 0) {
-            newId = 1
-        } else {
-            newId = objet[objet.length - 1].id + 1;
-        }
-
-        const newObj ={...data , id: newId}
-        objet.push (newObj);
-
-
         try {
-            await fs.writeFile(this.ruta, JSON.stringify(objet, null, 2))
-            return newId
+    
+          let objet = [await this.getAll()];
+    
+          let newId;
+    
+          if (objet.length === 0) {
+    
+            newId =1;
+    
+          } else {
+    
+            newId = objet[objet.length - 1]+ 1;
+    
+          }
+    
+    
+          const newObj = { ...data, id:newId };
+    
+          objet.push(newObj);
+    
+    
+          await fs.writeFile(this.ruta, JSON.stringify(objet, null, 2));
+    
+          
+    
         } catch (error) {
-            throw new Error(`Error al guardar: ${error}`)
+    
+          throw new Error(`Error al guardar: ${error}`);
+    
         }
-
-        
-
-    }
-
+    
+      }
     async getById(id) {
-        const objetos= await this.getAll();
-
+        try{
+        const objetos= [await this.getAll()];
+        
         const nuevoObjeto = objetos.find(elemento => elemento.id== id);
-        return nuevoObjeto;
+        return JSON.stringify(nuevoObjeto);
+    }
+        catch(error){
+            console.log(`Error en conseguir id ${error}`);
+
+        }
     }
 
     async getAll() {
         try {
            let objetos = await fs.readFile(this.ruta, 'utf-8');
 
-            objetos = JSON.parse(objetos);
+           let objetoss = JSON.stringify(objetos,null,2);
+            return objetoss;
 
         } catch (error) {
+            console.log(`Error al buscar la informacion ${error}`);
            return []
         }
     }
 
     async deleteById(id) {
+        try {
 
-        const objetos= await this.getAll();
+        const objetos= [await this.getAll()];
 
         const nuevoObjeto = objetos.filter(elemento => elemento.id !== id);
         if(nuevoObjeto.length === objetos.length){
             throw new Error('no se encontro el id')
-        }
-        try {
-            await fs.writeFile(this.ruta, JSON.stringify(nuevoObjeto,null,2));
+        }else{
+      
+            await fs.writeFile(this.ruta, JSON.stringify(nuevoObjeto,null,2));}
         } catch (error) {
-            
+            console.log(`eliminar ${error}`);
         }
 
     }
 
     async deleteAll() {
-        await fs.writeFile(`./${this.ruta}`, '');
+        try {
+            await fs.writeFile(`./${this.ruta}`, '');
+        }catch(error){
+            console.log(`Hubo un error ${error}`);
+        }
     }
 }
 const contenedor = new Contenedor('./productos.txt');
