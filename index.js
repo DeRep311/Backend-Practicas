be21f3919d929cdbba4863863e0944a26d48e45b
DeRep311 <77757535+DeRep311@users.noreply.github.com>
2022-08-20 20:21:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,28 +7,35 @@ const PORT=8080;
 const server=app.listen(PORT,()=>{
     console.log(`El servidor escucha en el puerto ${PORT}`);
 });
+const product= new Contenedor('./productos.txt');
+
+
+    
+
 
 app.get('/productos',async (req, res)=>{
     try{
-        const product= new Contenedor('./productos.txt');
-        let todo= await product.getAll();
         
-        res.send(`Los productos son ${todo}`);
+        
+        let todo=await product.getAll()
+        res.send(todo);
     }catch(error){
         res.status(500).send(errorazo);
     }
     
 })
 app.get('/productosRandom',async (req,res )=>{
-    try{
-    const product= new Contenedor('./productos.txt');
-    let todo=await product.getAll();
     
-        function randomnum(x=1,y=todo.length) {
-            return Math.random(x,y);
-        }
 
-        res.send(`Aqui se encuentra el producto random: ${todo[randomnum]}`);
+    try{
+       
+        let todo=await product.getAll()
+        function random(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+          }
+        
+
+        res.send(todo[random(0, todo.length-1)]);
 
 
 }catch(error){
